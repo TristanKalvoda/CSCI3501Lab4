f2d5371d82f1dc1d9ffdd4ba7a555944b33763ec
Dylan Packer <packe089@morris.umn.edu>
2024-09-12 14:29:59 -0500

    Implemented binApproximator need to add tests in comments

--- Main.java
+++ Main.java
@@ -5,14 +5,12 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String s[] = scanner.nextLine().split(" ");
         scanner.close();
-        System.out.println(Arrays.toString(s));
         int B = Integer.parseInt(s[0]);
         int N = Integer.parseInt(s[1]);
         int[] arr = new int[N];
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(s[i + 2]);
         }
-        System.out.println(Arrays.toString(arr));
 
         List<List<Integer>> bins = binApproximator(B, arr);
 
@@ -35,13 +33,45 @@ public class Main {
     }
 
     public static List<List<Integer>> binApproximator(int B, int[] arr) {
+        Arrays.sort(arr);
 
-        // // Sort items in decreasing order
-        // Arrays.sort(items);
-        // for (int i = 0; i < items.length / 2; i++) {
-        // int temp = items[i];
-        // items[i] = items[items.length - i - 1];
-        // items[items.length - i - 1] = temp;
-        // }
+        // Sort arr in decreasing order
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = temp;
+        }
+
+        List<List<Integer>> bins = new ArrayList<>();
+        List<Integer> unpackedItems = new ArrayList<>();
+
+        // First-Fit Decreasing Algorithm
+        for (int item : arr) {
+            boolean placed = false;
+            for (List<Integer> bin : bins) {
+                int currentLoad = bin.stream().mapToInt(Integer::intValue).sum();
+                if (currentLoad + item <= B) {
+                    bin.add(item);
+                    placed = true;
+                    break;
+                }
+            }
+            if (!placed) {
+                if (bins.size() < 3) {
+                    List<Integer> newBin = new ArrayList<>();
+                    newBin.add(item);
+                    bins.add(newBin);
+                } else {
+                    unpackedItems.add(item);
+                }
+            }
+        }
+
+        // Output unpacked items
+        System.out.println("Unpacked items: " + unpackedItems);
+
+        return bins;
+
     }
 }
